5a05229c1f05b274d08b2ad7ed7b1747940e1643
James Wilson <james@drakeapps.com>
2019-02-13 14:18:06 -0600

    Version 1.0

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>james.diet - james's diet</title>
+<style type="text/css">
+#main {
+	width:100%;
+	height: 100%;
+	text-align: center;
+}
+</style>
+</head>
+<body>
+<div id="main"><img src="pizza-slice.jpg" alt="mmmm...pizza" title="mmmm....pizza" /></div>
